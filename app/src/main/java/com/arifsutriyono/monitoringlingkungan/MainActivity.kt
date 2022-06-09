172555c431fab24da054fcd7281e8bdf0be151de
stryn26 <sutrionoarif@gmail.com>
2022-06-09 11:30:38 +0700

    master

--- app/src/main/java/com/arifsutriyono/monitoringlingkungan/MainActivity.kt
+++ app/src/main/java/com/arifsutriyono/monitoringlingkungan/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.arifsutriyono.monitoringlingkungan
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.Toast
 import com.arifsutriyono.monitoringlingkungan.databinding.ActivityMainBinding
-
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         binding.refresh.setOnClickListener{
             readData()
         }
+        setupLineChartData()
     }
 
     private fun readData(){
@@ -43,5 +48,48 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupLineChartData(){
+        val yVals = ArrayList<Entry>()
+
+        yVals.add(Entry(0f, 30f, "0"))
+        yVals.add(Entry(1f, 2f, "1"))
+        yVals.add(Entry(2f, 4f, "2"))
+        yVals.add(Entry(3f, 6f, "3"))
+        yVals.add(Entry(4f, 8f, "4"))
+        yVals.add(Entry(5f, 10f, "5"))
+        yVals.add(Entry(6f, 22f, "6"))
+        yVals.add(Entry(7f, 12.5f, "7"))
+        yVals.add(Entry(8f, 22f, "8"))
+        yVals.add(Entry(9f, 32f, "9"))
+        yVals.add(Entry(10f, 54f, "10"))
+        yVals.add(Entry(11f, 28f, "11"))
+
+        val set1: LineDataSet
+        set1 = LineDataSet(yVals, "DataSet 1")
+
+        set1.color = Color.BLUE
+        set1.setCircleColor(Color.BLUE)
+        set1.lineWidth = 1f
+        set1.circleRadius = 3f
+        set1.setDrawCircleHole(false)
+        set1.valueTextSize = 0f
+        set1.setDrawFilled(false)
+
+        val dataSets = ArrayList<ILineDataSet>()
+        dataSets.add(set1)
+        val data = LineData(dataSets)
+
+        // set data
+        lineChart.setData(data)
+        lineChart.description.isEnabled = false
+        lineChart.legend.isEnabled = false
+        lineChart.setPinchZoom(true)
+        lineChart.xAxis.enableGridDashedLine(5f, 5f, 0f)
+        lineChart.axisRight.enableGridDashedLine(5f, 5f, 0f)
+        lineChart.axisLeft.enableGridDashedLine(5f, 5f, 0f)
+        //lineChart.setDrawGridBackground()
+        lineChart.xAxis.labelCount = 11
+        lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+    }
 }
 
